c8fb1454bbbfd9c67a0797168634db9b3b958071
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2019-10-14 21:34:51 +0200

    Added generic template

---
+++ sketch_template/sketch_template.ino
@@ -0,0 +1,31 @@
+// definieer constanten voor je pinnen
+
+
+// definieer statussen
+
+
+// definieer eventuele overige globale variabelen
+
+
+
+void setup() {
+  // setup wordt eenmaal aangeroepen
+  // stel hier in of een digital poort INPUT of OUTPUT is
+
+
+  // om te debuggen moet je berichten
+  // naar je computer kunnen sturen:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // update de status van je sensor(s)
+
+
+  // update op basis van sensordata de actuatorstatus(sen)
+
+
+  // doorvoeren van actuatorstatussen
+
+
+}
